a31f8404a35e9974f1b9e40013940c5145399ec0
CodingalHarsha <sriharsha.dse@gmail.com>
2026-07-22 18:02:12 +0200

    Type Casting

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+Name = "Harsha"
+age = 13
+height = 6.1
+print("Name : ",Name, "\nDatatype of name is :", type(Name))
+print("age : ",age, "\nDatatype of age is :", type(age))
+print("height : ",height, "\nDatatype of height is :", type(height))
+
+print("*******************Type Casting**********************")
+age = str(age)
+height = int(height)
+
+print("age : ",age, "\nDatatype of age is :", type(age))
+print("height : ",height, "\nDatatype of height is :", type(height))
