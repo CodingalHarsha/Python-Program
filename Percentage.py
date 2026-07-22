00133dd1dea791b47156342a767cfee08354ab70
CodingalHarsha <sriharsha.dse@gmail.com>
2026-07-22 17:46:53 +0200

    Type

---
+++ Percentage.py
@@ -0,0 +1,6 @@
+Name = input("Enter name:")
+Science = int(input("Enter marks:"))
+Math = int(input("Enter marks:"))
+total = Science + Math
+average = total/2
+print(average)
---
+++ type.py
@@ -0,0 +1,11 @@
+num = 15
+print("type of num =", type(num))
+
+str = "Harsha"
+print("type of str :", type(str))
+
+age = 13.6
+print("type of num :", type(age))
+
+is_student = True
+print("type of bool :", type(is_student))
